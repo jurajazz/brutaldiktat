790309e6db992f2260ce1ee5f241033dcb222b21
Jurajazz <jurajazz@users.noreply.github.com>
2022-01-08 16:37:55 +0100

    Presun umiestnovania pismen do text.js

--- game1/src/diktatApp.js
+++ game1/src/diktatApp.js
@@ -401,83 +401,14 @@ function cursorGotoPreviousPosition()
 
 function showText()
 {
-	elapsed=0;
 	textContainer.removeChildren()
 	showBackground()
-	letters=[]
+	elapsed = 0
 	currentCursorIndex = 0
-	var wordListChallenge = TEXT.generateNewText()
-  	var wordListJoined = wordListChallenge.join(', ')
-	console.log("showText: "+wordListJoined)
-
-	let max_lines_count=8;
-	let line_size_y=30;
-	let leftx=-200;
-	let rightx=200;
-	let basex=leftx;
-	let basey=-line_size_y*max_lines_count/2;
-	let index=0;
-	for (let i = 0; i < wordListJoined.length; i++)
-	{
-		var char=wordListJoined[i];
-		var char_alternative='i';
-		var should_be_ypsilon=false;
-		var can_be_any_iy=false;
-		var is_wildcard=false;
-		var color = 0x606060;
-		var is_long=false;
-		if (char=='y' || char=='ý') should_be_ypsilon = true;
-		if (char=='i' || char=='y' || char==TEXT.KTOREKOLVEK_IY_KRATKE)
-		{
-			is_wildcard=true;
-			index++;
-		}
-		if (char=='í' || char=='ý' || char==TEXT.KTOREKOLVEK_IY_DLHE)
-		{
-			is_wildcard=true;
-			is_long=true;
-			char_alternative='í';
-		}
-		if (is_wildcard)
-		{
-			color = 0x8000ff;
-			char='y';
-			if (is_long) char='ý';
-		}
-		if (char==TEXT.KTOREKOLVEK_IY_KRATKE || char==TEXT.KTOREKOLVEK_IY_DLHE) can_be_any_iy = true;
-		let letter_object = new LETTER.Letter(char,char_alternative,color,is_wildcard,is_long,index,should_be_ypsilon,can_be_any_iy);
-		//letter_object.spos(basex,basey)
-		letter_object.setPosition(basex,basey)
-		letters.push(letter_object)
-		// generuj poziciu dalsieho pismena
-		basex += letter_object.getWidth()
-		if (basex>rightx)
-		{
-			// prechod na novi riadok
-			// hladaj poslednu medzeru
-			let pocetPismenNaPresun=0;
-			let id=i;
-			while (letters[id].getStructure().char!=' ')
-			{
-				id--;
-				pocetPismenNaPresun++;
-				if (id==0) break; // ak bi tam nahodou nebola
-			}
-			if (pocetPismenNaPresun>0) id++;
-			// posun slovo z predchadzajuceho riadku na novi riadok
-			basex=leftx;
-			basey+=line_size_y;
-			for (let j=0;j<pocetPismenNaPresun;j++)
-			{
-				//console.log("Presun pismeno "+letters[id+j].char);
-				let l=letters[id+j]
-				l.setPosition(basex,basey)
-				basex+=l.getWidth();
-			}
-		}
-	}
+	letters=[]
+	TEXT.placeLetters(letters)
 	letters.forEach(addLetterToContainer);
-	addLettersAnimation();
+	addAnimations();
 }
 
 function addLetterToContainer(letter)
@@ -489,7 +420,7 @@ function addLetterToContainer(letter)
 	}
 }
 
-function addLettersAnimation()
+function addAnimations()
 {
 	// nastav polohu nedefinovanich pismen
 	letters.forEach(setLettersPosition);
--- game1/src/text.js
+++ game1/src/text.js
@@ -1,4 +1,5 @@
 import diktatData from "../assets/diktat-data.yaml"
+import * as LETTER from './animation/letter'
 
 export const KTOREKOLVEK_IY_KRATKE = 'ǒ'
 export const KTOREKOLVEK_IY_DLHE = 'Ǒ'
@@ -30,4 +31,77 @@ export function generateNewText ()
 	)
 
   return wordListChallenge
+}
+
+export function placeLetters(letters)
+{
+	var wordListChallenge = generateNewText()
+	var wordListJoined = wordListChallenge.join(', ')
+
+	let max_lines_count=8;
+	let line_size_y=30;
+	let leftx=-200;
+	let rightx=200;
+	let basex=leftx;
+	let basey=-line_size_y*max_lines_count/2;
+	let index=0;
+	for (let i = 0; i < wordListJoined.length; i++)
+	{
+		var char=wordListJoined[i];
+		var char_alternative='i';
+		var should_be_ypsilon=false;
+		var can_be_any_iy=false;
+		var is_wildcard=false;
+		var color = 0x606060;
+		var is_long=false;
+		if (char=='y' || char=='ý') should_be_ypsilon = true;
+		if (char=='i' || char=='y' || char==KTOREKOLVEK_IY_KRATKE)
+		{
+			is_wildcard=true;
+			index++;
+		}
+		if (char=='í' || char=='ý' || char==KTOREKOLVEK_IY_DLHE)
+		{
+			is_wildcard=true;
+			is_long=true;
+			char_alternative='í';
+		}
+		if (is_wildcard)
+		{
+			color = 0x8000ff;
+			char='y';
+			if (is_long) char='ý';
+		}
+		if (char==KTOREKOLVEK_IY_KRATKE || char==KTOREKOLVEK_IY_DLHE) can_be_any_iy = true;
+		let letter_object = new LETTER.Letter(char,char_alternative,color,is_wildcard,is_long,index,should_be_ypsilon,can_be_any_iy);
+		//letter_object.spos(basex,basey)
+		letter_object.setPosition(basex,basey)
+		letters.push(letter_object)
+		// generuj poziciu dalsieho pismena
+		basex += letter_object.getWidth()
+		if (basex>rightx)
+		{
+			// prechod na novi riadok
+			// hladaj poslednu medzeru
+			let pocetPismenNaPresun=0;
+			let id=i;
+			while (letters[id].getStructure().char!=' ')
+			{
+				id--;
+				pocetPismenNaPresun++;
+				if (id==0) break; // ak bi tam nahodou nebola
+			}
+			if (pocetPismenNaPresun>0) id++;
+			// posun slovo z predchadzajuceho riadku na novi riadok
+			basex=leftx;
+			basey+=line_size_y;
+			for (let j=0;j<pocetPismenNaPresun;j++)
+			{
+				//console.log("Presun pismeno "+letters[id+j].char);
+				let l=letters[id+j]
+				l.setPosition(basex,basey)
+				basex+=l.getWidth();
+			}
+		}
+	}
 }
