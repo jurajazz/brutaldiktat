8d04a4b52cef647f7af903d9681c848f194d4156
Jurajazz <spam@organic.sk>
2022-01-29 23:42:32 +0100

    Zmenene farbi z fialovej na modre.

--- game1/src/styles.js
+++ game1/src/styles.js
@@ -3,11 +3,13 @@ export const fontFamily = 'Encode Sans Condensed'
 export const buttonText = {
     fontFamily: fontFamily,
     fontSize: 24,
-    fill: 0x8000ff,
-    stroke: '#333333',
-    strokeThickness: 1,
+    fontWeight: "bold",
+    //fontStyle: 'italic',
+    fill: 0x66A0CC,
+    //stroke: '#66A0CC',
+    //strokeThickness: 1,
     align: 'center'
 }
 
 export const buttonBackGround = 0xC0C0C0
-export const colorWildCardLetter = 0x8000ff
+export const colorWildCardLetter = 0x66A0CC
