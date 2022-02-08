cb24cfad41d1563fb3c410bbe7bf926c95495a60
Jurajazz <spam@organic.sk>
2022-02-08 07:18:47 +0100

    Pridana moznost zobrazovat stvorce namiesto animovanich iota/ipsilon.

--- game1/src/animation/letter.js
+++ game1/src/animation/letter.js
@@ -13,6 +13,12 @@ export function disableRender()
 	render_enabled = false
 }
 
+var use_squares=0 // zobrazuje stvorce namiesto hojdajucich pismen
+export function setUseSquares(squares)
+{
+	use_squares = squares
+}
+
 export class Letter
 {
 	constructor(char,char_alternative,color,is_wildcard,is_long,index,should_be_ypsilon,can_be_any_iy,size)
@@ -20,6 +26,7 @@ export class Letter
 		this.structure = {
 			sprite: null,
 			sprite2: null,
+			square: null,
 			char: char,
 			answer: char,
 			is_wildcard: is_wildcard,
@@ -36,6 +43,18 @@ export class Letter
 		}
 		if (render_enabled)
 		{
+			if (is_wildcard && use_squares)
+			{
+				// stvorec namiesto pismena
+				var square_size = size*0.5
+				var line_size = size*0.07
+				this.structure.square = new PIXI.Graphics()
+				var box=this.structure.square;
+				box.beginFill(STYLES.colorWildCardLetter,0.3);
+				//box.lineStyle(line_size, STYLES.colorWildCardLetter,0.1);
+				box.drawRoundedRect(0, 0, square_size*0.5, square_size, line_size);
+				box.endFill();
+			}
 			this.structure.sprite = new PIXI.Text(char,
 			{
 				fontFamily : STYLES.fontFamily,
@@ -85,6 +104,7 @@ export class Letter
 		if (!letter.is_wildcard) return;
 		let s=letter.sprite
 		let s2=letter.sprite2
+		let sq=letter.square
 		if (letter.is_selected)
 		{
 			s.alpha = 0
@@ -95,23 +115,35 @@ export class Letter
 				s.alpha = letter.alpha_max_selected
 			else
 				s2.alpha = letter.alpha_max_selected
+			if (use_squares)
+				letter.square.alpha = 0
 		}
 		else
 		{
 			let angle=10*Math.cos(elapsed / 13.0+letter.index)
 			s.angle = angle
 			s2.angle = angle
-			// viditelnost medzi sprite a sprite2
-			if (TEXT.is_new_orthography)
+			if (use_squares)
 			{
+				sq.alpha = letter.alpha_max_selected
+				sq.angle = angle
 				s.alpha = 0
-				s2.alpha = letter.alpha_max
+				s2.alpha = 0
 			}
 			else
 			{
-				let alpha = letter.alpha_max*(0.5+0.5*Math.cos(elapsed / 20.0 + 2.5*letter.index))
-				s.alpha = alpha
-				s2.alpha = letter.alpha_max-alpha
+				// viditelnost medzi sprite a sprite2
+				if (TEXT.is_new_orthography)
+				{
+					s.alpha = 0
+					s2.alpha = letter.alpha_max
+				}
+				else
+				{
+					let alpha = letter.alpha_max*(0.5+0.5*Math.cos(elapsed / 20.0 + 2.5*letter.index))
+					s.alpha = alpha
+					s2.alpha = letter.alpha_max-alpha
+				}
 			}
 		}
 	}
@@ -133,11 +165,17 @@ export class Letter
 		if (!render_enabled) return
 		var s=this.structure.sprite
 		var s2=this.structure.sprite2
+		let sq = this.structure.square
 		s.x = x;
 		s2.x = x;
 		s.y = y;
 		s2.y = y;
 		s2.alpha = 0; // neviditelna alternativa
+		if (sq)
+		{
+			sq.x = x;
+			sq.y = y;
+		}
 	}
 	initPosition()
 	{
@@ -146,14 +184,21 @@ export class Letter
 		var letter = this.structure
 		if (!letter.is_wildcard) return
 		let s = letter.sprite
+		let s2 = letter.sprite2
+		let sq = letter.square
+		if (sq)
+		{
+			sq.pivot.set(s.width*0.3,s.height*0.3);
+			sq.x+=s.width*0.5;
+			sq.y+=s.height*0.7;
+		}
 		//let s=letter.sprite;
 		s.pivot.set(s.width/2,s.height/2);
 		s.x+=s.width/2;
 		s.y+=s.height/2;
 		s.scale.set(0.9,0.9);
-		s = letter.sprite2
-		s.pivot.set(s.width/2,s.height/2);
-		s.x+=s.width*1;
-		s.y+=s.height/2;
+		s2.pivot.set(s.width/2,s.height/2);
+		s2.x+=s.width*0.8;
+		s2.y+=s.height/2;
 	}
 }
